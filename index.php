e3505021c443e2e1aff2f886fe3b70964797d784
root <root@localhost.localdomain>
2017-07-28 08:59:17 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Blog de Michael</title>
+</head>
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+<body>
+<nav class="navbar navbar-inverse navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Abad-05</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="#about">Entrada</a></li>
+            <li><a href="#contact">Favoritos</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Proyectos <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Lenguajes de programacion</a></li>
+                <li><a href="#">Java</a></li>
+                <li><a href="#">Python</a></li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-header">Frameworks</li>
+                <li><a href="#">Phalcon</a></li>
+                <li><a href="#">Laravel</a></li>
+              </ul>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span>Iniciar sesion</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Registro</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <!--  filas responsi-->
+    <div class="container">
+    	<div class="jumbotron">
+    		<h1>Blog de Michael</h1>
+    		<p>Blog dedicado a la programacion y desarrolo</p>
+    	</div>
+    </div>
+
+    <div class="container">
+    	<div class="row">
+    		<div class="col-md-4">
+    			<div class="row">
+    				<div class="col-md-12">
+    					<div class="panel panel-default">
+    						<div class="panel-heading">
+    							<span class="glyphicon glyphicon-search" aria-hidden="true"></span> Busqueda
+    						</div>
+    						<div class="panel-body">
+    							<div class="form-group">
+    								<input type="text" class="form-control" placeholder="¿Que buscas?" name="">
+    							</div>
+    							<button class="form-control">Buscar</button>
+    						</div>
+    					</div>
+    				</div>
+    			</div>
+    			<div class="row">
+    				<div class="col-md-12">
+    					<div class="panel panel-default">
+    						<div class="panel-heading">
+    							<span class="glyphicon glyphicon-filter" aria-hidden="true"></span>Filtro
+    						</div>
+    						<div class="panel-body">
+    							
+    						</div>
+    					</div>
+    				</div>
+    			</div>
+    			<div class="row">
+    				<div class="col-md-12">
+    					<div class="panel panel-default">
+    						<div class="panel-heading">
+    							<span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>Archivo
+    						</div>
+    						<div class="panel-body">
+    							
+    						</div>
+    					</div>
+    				</div>
+    			</div>
+    		</div>
+    		<div class="col-md-8">
+    			<div class="panel panel-default">
+    				<div class="panel-heading">
+    					<span class="glyphicon glyphicon-time" aria-hidden="true"></span>Ultimas entradas
+    				</div>
+    				<div class="panel-body">
+    					<P>Todavia  no hay entradas que mostrar</P>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>

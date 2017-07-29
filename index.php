baf5529dc54e84ded95d0eb6f49cb50c96a03aa9
root <root@localhost.localdomain>
2017-07-29 00:15:19 -0500

    segundo comit

--- index.php
+++ index.php
@@ -1,50 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Blog de Michael</title>
-</head>
-  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  	<link rel="stylesheet" type="text/css" href="css/estilos.css">
-<body>
-<nav class="navbar navbar-inverse navbar-static-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Abad-05</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li><a href="#about">Entrada</a></li>
-            <li><a href="#contact">Favoritos</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Proyectos <span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Lenguajes de programacion</a></li>
-                <li><a href="#">Java</a></li>
-                <li><a href="#">Python</a></li>
-                <li role="separator" class="divider"></li>
-                <li class="dropdown-header">Frameworks</li>
-                <li><a href="#">Phalcon</a></li>
-                <li><a href="#">Laravel</a></li>
-              </ul>
-            </li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#"><span class="glyphicon glyphicon-user"></span>Iniciar sesion</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Registro</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
+<?php
+include_once"app/Conexion.php";
+include_once"app/RepositorioUsuario.php";
 
+$titulo="Abad-05";//nombre del titulo de la pagina web
+
+include_once"plantillas/cabecera.php";
+include_once"plantillas/navbar.php";
+?>
     <!--  filas responsi-->
     <div class="container">
     	<div class="jumbotron">
@@ -108,8 +70,4 @@
     		</div>
     	</div>
     </div>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</body>
-</html>
+<?php include_once"plantillas/pie.php";?>
